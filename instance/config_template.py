68704ac83191e81e4b6f7c10a7f5751ad4e30c5e
Sascha Kiefer <mail@sascha-kiefer.de>
2020-04-04 11:13:55 +0200

    Adding config.py template

---
+++ instance/config_template.py
@@ -0,0 +1,10 @@
+# Copy this file to config.py an configure the secrets
+GITHUB_LOGON_LINK = "https://github.com/login/oauth/authorize"
+GITHUB_TOKEN_EXCHANGE_LINK = "https://github.com/login/oauth/access_token" GITHUB_USER_LINK = "https://api.github.com/user"
+
+STRAP_ISSUES_URL_DEFAULT = "https://github.com/saschakiefer/PyStrap/issues/new""
+
+GITHUB_CLIENT_ID = "<Your Client ID>"
+GITHUB_CLIENT_SECRET = "<Your Client Secret>"
+
+APP_SECRET = "generate with: import secrets\nsecrets.token_urlsafe(32)"
