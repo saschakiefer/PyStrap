5a02055d5d5f16dbc62ccad9cc625d4f2954a24d
Sascha Kiefer <mail@sascha-kiefer.de>
2020-04-04 23:29:41 +0200

    Add Catalina to the Version Check

--- generator/static/strap.sh
+++ generator/static/strap.sh
@@ -84,7 +84,7 @@ escape() {
 
 MACOS_VERSION="$(sw_vers -productVersion)"
 echo "$MACOS_VERSION" | grep $Q -E "^10.(9|10|11|12|13|14)" || {
-  abort "Run Strap on macOS 10.9/10/11/12/13/14."
+  abort "Run Strap on macOS 10.9/10/11/12/13/14/15."
 }
 
 [ "$USER" = "root" ] && abort "Run Strap as yourself, not root."
